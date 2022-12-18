9a45bb5772b32496bd4e552889ebd26ec150ee97
Deepanshu0810 <deepanshu0810@gmail.com>
2022-12-18 10:53:10 +0530

    ADD: api-call

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+import requests
+from flask import Flask,render_template,url_for
+import creds
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Weather App Comming soon"
+    city='London'
+    api = f'http://api.openweathermap.org/data/2.5/weather?q={city},uk&APPID={creds.api_key}'
+    r=requests.get(api).json()
+    print(r)
+    return render_template("weather.html")
+
 
 if __name__=="__main__":
     app.run(debug=True)
