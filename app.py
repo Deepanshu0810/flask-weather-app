da942ae87309b52a79bc2489ed4feaeb09d70e8a
Deepanshu0810 <deepanshu0810@gmail.com>
2023-01-28 16:11:06 +0530

    UPDATE:hide-api-key

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import requests
 from flask import Flask,render_template,url_for
-import creds
+import os
+
+api_key = os.environ.get('API_KEY')
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
     city='London'
-    api = f'http://api.openweathermap.org/data/2.5/weather?q={city},uk&APPID={creds.api_key}'
+    api = f'http://api.openweathermap.org/data/2.5/weather?q={city},uk&APPID={api_key}'
     r=requests.get(api).json()
-    print(r)
+
+    if(r['cod']=='404'):
+        weather = {'city' : 'City Not Found'}
+    else:
+        weather ={
+            'city' : city,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon']
+        }
+
+    print(weather)
     return render_template("weather.html")
 
 
